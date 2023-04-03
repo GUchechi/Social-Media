cb560c5f91a0c7c40e6f147c1fb6976441f78d14
GUchechi <godswilluchendu7@gmail.com>
2023-04-03 03:44:15 +0100

    followers Card

--- src/components/FollowersCard/FollowersCard.jsx
+++ src/components/FollowersCard/FollowersCard.jsx
@@ -1,11 +1,24 @@
-import './FollowersCard.css'
+import { Followers } from "../../Data/FollowersData";
+import "./FollowersCard.css";
 
 const FollowersCard = () => {
   return (
-    <div className='followersCard'>
-        <h3>Who is following you?</h3>
-    </div>
-  )
-}
+    <div className="followersCard">
+      <h3>Who is following you?</h3>
 
-export default FollowersCard
+      {Followers.map((follower, id) => (
+        <div className="follower">
+          <div>
+            <img src={follower.img} alt="followerImg" className="followerImg" />
+            <div className="followerName">
+              <span>{follower.name}</span>
+              <span>{follower.username}</span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FollowersCard;
