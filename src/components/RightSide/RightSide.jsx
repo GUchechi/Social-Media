e1e7cba2554f00178bd99201cbb0d643a82ffd81
GUchechi <godswilluchendu7@gmail.com>
2023-04-05 09:57:28 +0100

    refactoring

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Home from "../../img/home.png";
 import Noti from "../../img/noti.png";
 import Comment from "../../img/comment.png";
+import { UilEstate } from '@iconscout/react-unicons'
 import { UilSetting } from "@iconscout/react-unicons";
 import "./RightSide.css";
 import TrendCard from "../TrendCard/TrendCard";
@@ -13,8 +14,8 @@ const RightSide = () => {
   return (
     <div className="rightSide">
       <div className="navIcons">
-        <img src={Home} alt="" />
-        <UilSetting />
+        <UilEstate  size='26.5'/>
+        <UilSetting size='26.5' />
         <img src={Noti} alt="" />
         <img src={Comment} alt="" />
       </div>
