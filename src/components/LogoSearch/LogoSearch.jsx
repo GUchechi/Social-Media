55fa8d88c9ef8f4f790c824cfca10033079ab962
GUchechi <godswilluchendu7@gmail.com>
2023-04-05 09:41:16 +0100

    logoSearch

--- src/components/LogoSearch/LogoSearch.jsx
+++ src/components/LogoSearch/LogoSearch.jsx
@@ -1,19 +1,19 @@
-import Logo from '../../img/logo.png'
-import {UilSearch} from '@iconscout/react-unicons'
-import './LogoSearch.css'
+import { UilSearch } from "@iconscout/react-unicons";
+import { UilShutter } from "@iconscout/react-unicons";
+import "./LogoSearch.css";
 
 const LogoSearch = () => {
   return (
-    <div className='logoSearch'>
-      <img src={Logo} alt="logoImg" />
+    <div className="logoSearch">
+      <UilShutter width="45" height="50" style={{ color: "var(--blue)" }} />
       <div className="search">
-        <input type="text" placeholder='#Explore'/>
+        <input type="text" placeholder="#Explore" />
         <div className="s-icon">
           <UilSearch />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LogoSearch
+export default LogoSearch;
