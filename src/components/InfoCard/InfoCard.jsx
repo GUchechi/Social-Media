fe89034586eac69b99e10a27366b7a90ea2b52ee
GUchechi <godswilluchendu7@gmail.com>
2023-04-09 23:09:57 +0100

    info card

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -41,7 +41,7 @@ const InfoCard = () => {
         <span>
           <b> Works at </b>
         </span>
-        <span>MicroSoft</span>
+        <span> MicroSoft</span>
       </div>
       <button className="button logout-button">Logout</button>
     </div>
